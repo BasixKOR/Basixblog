3ca9f1c7f3df9139f59188821244f4a733c23b78
Basix <basix@basix.tech>
2019-08-01 15:52:49 +0900

    Add gatsby-plugin-root-import

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
       }
     },
     'gatsby-plugin-typescript',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-root-import'
   ]
 }
